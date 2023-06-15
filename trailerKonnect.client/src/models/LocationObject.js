38892067aad949c93901a329771a1b6592b6fd8b
Thomas Peek <peekthomas369@icloud.com>
2023-06-14 21:10:13 -0600

    a

---
+++ trailerKonnect.client/src/models/LocationObject.js
@@ -0,0 +1,10 @@
+export class LocationObject {
+    constructor(data){
+        this.street = data.street
+        this.city = data.city
+        this.state = data.state
+        this.zip = data.zip
+        this.county = data.county
+
+    }
+}
---
+++ trailerKonnect.client/src/models/PaymentObject.js
@@ -0,0 +1,5 @@
+export class PaymentObject {
+    constructor(data) {
+        
+    }
+}
